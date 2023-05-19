c850d1576c7622377fafd7ecbefae7a7e5fbb03d
sam030398 <90159360+sam030398@users.noreply.github.com>
2023-05-19 11:38:16 +0800

    Update langchain_sql_cloudrun.py

--- langchain_sql_cloudrun.py
+++ langchain_sql_cloudrun.py
@@ -116,7 +116,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET"])
 def main():
-  my_secret = access_secret_version("projects/354659879420/secrets/openai/versions/1")
+  os.environ["OPENAI_API_KEY"] = access_secret_version("projects/354659879420/secrets/openai/versions/1")
   llm = ChatOpenAI(model_name="gpt-3.5-turbo")
   project_id = request.args.get("project_id")
   dataset_id = request.args.get("dataset_id")
